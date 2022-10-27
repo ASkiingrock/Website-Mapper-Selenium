2c93a3ec98372ebe2258c7e037b2c8c947bc5533
ASkiingrock <93888719+ASkiingrock@users.noreply.github.com>
2022-10-27 18:51:36 +1100

    Update start-tree.py

--- start-tree.py
+++ start-tree.py
@@ -3,8 +3,6 @@ from anytree.exporter import JsonExporter
 import json
 
 link = 'YOUR WEBSITE'
-if link[-1] != "/"
-    link += "/
 root = AnyNode(name=link)
 exporter = JsonExporter(sort_keys=True)
 exporttree = exporter.export(root)
